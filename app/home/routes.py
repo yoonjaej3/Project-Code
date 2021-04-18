0645f581dd54aa044e57e16039a25e9093063e7a
yoonjaej3 <yoonjaej3@gmail.com>
2021-04-18 22:57:26 +0900

    구매자 주문정보

--- app/home/routes.py
+++ app/home/routes.py
@@ -38,6 +38,63 @@ def store_info():
     
     return render_template('store_info.html',data_list=data_list)
 
+@blueprint.route('/order_detail')
+@login_required
+def order_detail():
+
+#  - 구매자 이름(users.user_name) one
+#  - 구매자 연락처(users.phone_number) one
+#  - 음식 이름(menu.menu_name)  two 3.store_id
+#  - 주문 시간(orders.order_time) three 1.user_id
+#  - 총 가격(orders.total_price) three 1.user_id
+#  - 음식 수량(orders.total_qty) three 1.user_id
+#  - 결제 종류(default = Credit Card)(orders.payment) three 1.user_id
+#  - 상태 (버튼 클릭시 상태 변경)(order_detail.state) four 3.order_id
+#  - 요청 사항(orders.requests) three 1.user_id
+    db = pymysql.connect(host="localhost", user="root", password="1234",
+                        db="mydb", charset="utf8")
+    cur = db.cursor()
+
+    
+    data_list_two=() 
+    data_list_four=()
+    data_list_three=()
+    user_id_list=[]
+    sql = "SELECT user_id,user_name,phone_number from users"
+    cur.execute(sql)
+    data_list_one = cur.fetchall()
+    
+    for i in data_list_one:
+        user_id_list.append(i[0])
+
+    for i in user_id_list:
+        sql = '''SELECT A.user_name,A.phone_number,B.store_id,B.order_id,B.order_time,B.total_price,B.total_qty,B.payment,B.requests 
+        from users A INNER JOIN orders B
+        ON A.user_id=B.user_id
+        where B.user_id=%s
+        '''
+        cur.execute(sql,i)
+        data_list_two+= cur.fetchall()   #1~4
+
+    for i in data_list_two:
+        sql = "SELECT menu_name from menu where store_id=%s"
+        cur.execute(sql,i[2])
+        data_list_three+= cur.fetchall() 
+
+    for i in data_list_two:
+        sql = "SELECT order_state from order_detail where order_id=%s"
+        cur.execute(sql,i[3])
+        data_list_four+= cur.fetchall()   #2~5
+
+   
+    
+    data_list=[]
+    for i,j,k in zip(data_list_two,data_list_three,data_list_four):
+        data_list.append(i+j+k);
+   
+    print(data_list)
+    return render_template('order_detail.html',data_list=data_list)
+
 @blueprint.route('/store_save')
 @login_required
 def store_save():
