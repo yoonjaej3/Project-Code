081c479d8b7827eaf3b6f556467ed4078bfdc330
juthor <jemmaa1@naver.com>
2021-04-20 11:01:54 +0900

    test

---
+++ app/base/forms.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import TextField, PasswordField
+from wtforms.validators import InputRequired, Email, DataRequired
+
+## login and registration
+
+class LoginForm(FlaskForm):
+    username = TextField    ('Username', id='username_login'   , validators=[DataRequired()])
+    password = PasswordField('Password', id='pwd_login'        , validators=[DataRequired()])
+
+class CreateAccountForm(FlaskForm):
+    username = TextField('Username'     , id='username_create' , validators=[DataRequired()])
+    email    = TextField('Email'        , id='email_create'    , validators=[DataRequired(), Email()])
+    password = PasswordField('Password' , id='pwd_create'      , validators=[DataRequired()])
+
+# 주최자등록폼
+class RegisterOrganizationForm(FlaskForm):
+    company_name = TextField('Organization'     , id='company_name' , validators=[DataRequired(), InputRequired()])
+    manager_name = TextField('Manager'          , id='manager_name', validators=[DataRequired(), InputRequired()])
+    manager_contact = TextField('Contact'    , id='manager_contact', validators = [InputRequired()])
---
+++ app/home/routes.py
@@ -0,0 +1,158 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from app.home import blueprint
+from flask import render_template, redirect, url_for, request, jsonify
+from flask_login import login_required, current_user
+from app import login_manager
+from jinja2 import TemplateNotFound
+import pymysql
+
+config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'password': 'root0127:)',
+    'database': 'mydb'
+}
+
+@blueprint.route('/jaesung_festivalList')
+@login_required
+def index():
+
+    db = pymysql.connect(**config)
+    cur = db.cursor()
+    sql = "SELECT * from festival"
+    cur.execute(sql)
+
+    data_list = cur.fetchall()
+    
+    return render_template('jaesung_festivalList.html', segment='index', data_list=data_list)
+
+
+@blueprint.route('/jan_festival')
+@login_required
+def index2():
+
+    db = pymysql.connect(**config)
+    cur = db.cursor()
+    sql = "SELECT * from festival"
+    cur.execute(sql)
+
+    data_list = cur.fetchall()
+    
+    return render_template('jan_festival.html', segment='index2', data_list=data_list)
+
+@blueprint.route('/jan_apply')
+@login_required
+def index2_1():
+
+    db = pymysql.connect(**config)
+    cur = db.cursor()
+    sql = "SELECT * from organization"
+    cur.execute(sql)
+
+    data_list = cur.fetchall()
+    
+    return render_template('jan_apply.html', segment='index2_1', data_list=data_list)
+
+
+@blueprint.route('/juthor_dash')
+@login_required
+def index3():
+
+    db = pymysql.connect(**config)
+    cur = db.cursor()
+    sql = "SELECT * from festival"
+    cur.execute(sql)
+
+    data_list = cur.fetchall()
+    
+    return render_template('juthor_dash.html', segment='index3', data_list=data_list)
+
+
+@blueprint.route('/jhj_order')
+@login_required
+def order():
+    conn = pymysql.connect(**config)
+    cursor = conn.cursor()
+
+    sql = '''SELECT total_price FROM orders WHERE order_id=3'''
+
+    cursor.execute(sql)
+
+    data_list = cursor.fetchall()
+
+    return render_template('jhj_order.html', data_list=data_list)
+
+
+@blueprint.route('/order_post', methods=['POST'])
+@login_required
+def order_post():
+    json_data = request.get_json()
+
+    conn = pymysql.connect(**config)
+
+    try:
+        with conn.cursor() as cursor:
+            sql = "UPDATE users SET phone_number=%s WHERE user_id=3"
+            cursor.execute(sql, [json_data['phone_number']])
+
+
+        conn.commit()
+
+        with conn.cursor() as cursor:
+            sql = "UPDATE orders SET requests=%s WHERE order_id=3"
+            cursor.execute(sql, [json_data['request_text']])
+
+        conn.commit()
+
+    finally:
+        conn.close()
+
+    return jsonify(result = "success", result2= json_data)
+
+
+# @blueprint.route('/jhj_credit', methods=['POST'])
+# @login_required
+# def credit_get():
+
+
+
+@blueprint.route('/<template>')
+@login_required
+def route_template(template):
+
+    try:
+
+        if not template.endswith( '.html' ):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment( request )
+
+        # Serve the file (if exists) from app/templates/FILE.html
+        return render_template( template, segment=segment )
+
+    except TemplateNotFound:
+        return render_template('page-404.html'), 404
+    
+    except:
+        return render_template('page-500.html'), 500
+
+# Helper - Extract current page name from request 
+def get_segment( request ): 
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment    
+
+    except:
+        return None  
