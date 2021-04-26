22eacd4d978bed86476521b5712bcf016bc2d492
Jaden7856 <jhg7856@gmail.com>
2021-04-26 16:18:48 +0900

    Everything Error Solution

--- app/home/routes.py
+++ app/home/routes.py
@@ -14,8 +14,8 @@ from datetime import datetime
 
 
 config = {
-    'host': '127.0.0.1',
-    'port': 13306,
+    'host': '172.20.0.3',
+    'port': 3306,
     'user': 'root',
     'database': 'mydb',
     'charset': 'utf8'
